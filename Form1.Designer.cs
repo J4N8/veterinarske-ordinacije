f5d7ad14ae7ca2f7358b44be77c3dd2e07b3ad59
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-01-04 15:14:06 +0100

    Added npgsql package Added list to display kraji

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,20 +29,48 @@ namespace veterinarske_ordinacije
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxKraji = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxKraji
+            // 
+            this.listBoxKraji.FormattingEnabled = true;
+            this.listBoxKraji.ItemHeight = 20;
+            this.listBoxKraji.Location = new System.Drawing.Point(13, 13);
+            this.listBoxKraji.Name = "listBoxKraji";
+            this.listBoxKraji.Size = new System.Drawing.Size(544, 684);
+            this.listBoxKraji.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.AutoSize = true;
+            this.btnRefresh.Location = new System.Drawing.Point(627, 312);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(76, 30);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1412, 703);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listBoxKraji);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxKraji;
+        private System.Windows.Forms.Button btnRefresh;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace veterinarske_ordinacije
 {
@@ -15,6 +16,23 @@ namespace veterinarske_ordinacije
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            using (NpgsqlConnection con = new NpgsqlConnection("Server=rogue.db.elephantsql.com; User Id=ercqedby;" + "Password=GtZ43Cgtya5QVYjQLsVKUw8TUX8elf0k; Database=ercqedby;"))
+            {
+                con.Open();
+                NpgsqlCommand com = new NpgsqlCommand("SELECT posta, ime FROM kraji", con);
+                NpgsqlDataReader reader = com.ExecuteReader();
+                while (reader.Read())
+                {
+                    string posta = reader.GetString(0);
+                    string ime = reader.GetString(1);
+                    listBoxKraji.Items.Add(ime + ", " + posta);
+                }
+                con.Close();
+            }
         }
     }
 }
