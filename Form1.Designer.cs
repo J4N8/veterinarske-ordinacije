bb317ffe52e94708ffc5637b306843d5b797544a
J4N8 <43821141+J4N8@users.noreply.github.com>
2021-01-11 12:53:33 +0100

    Added new entry and editing area for Ordinacije

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,16 +36,29 @@ namespace veterinarske_ordinacije
             this.groupBoxSearch = new System.Windows.Forms.GroupBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.listBoxZaposleni = new System.Windows.Forms.ListBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageOrdinacije = new System.Windows.Forms.TabPage();
+            this.tabPageZaposleni = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxOrdinacijaIme = new System.Windows.Forms.TextBox();
+            this.textBoxOrdinacijaNaslov = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnOrdinacijeVnos = new System.Windows.Forms.Button();
+            this.btnOrdinacijeUpdate = new System.Windows.Forms.Button();
             this.groupBoxSearch.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPageOrdinacije.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBoxKraji
             // 
             this.listBoxKraji.FormattingEnabled = true;
             this.listBoxKraji.ItemHeight = 20;
-            this.listBoxKraji.Location = new System.Drawing.Point(562, 78);
+            this.listBoxKraji.Location = new System.Drawing.Point(568, 78);
             this.listBoxKraji.Name = "listBoxKraji";
-            this.listBoxKraji.Size = new System.Drawing.Size(544, 284);
+            this.listBoxKraji.Size = new System.Drawing.Size(550, 404);
             this.listBoxKraji.TabIndex = 0;
             // 
             // listBoxOrdinacije
@@ -54,7 +67,7 @@ namespace veterinarske_ordinacije
             this.listBoxOrdinacije.ItemHeight = 20;
             this.listBoxOrdinacije.Location = new System.Drawing.Point(12, 78);
             this.listBoxOrdinacije.Name = "listBoxOrdinacije";
-            this.listBoxOrdinacije.Size = new System.Drawing.Size(544, 284);
+            this.listBoxOrdinacije.Size = new System.Drawing.Size(550, 404);
             this.listBoxOrdinacije.TabIndex = 2;
             // 
             // comboBoxKraji
@@ -103,10 +116,117 @@ namespace veterinarske_ordinacije
             // 
             this.listBoxZaposleni.FormattingEnabled = true;
             this.listBoxZaposleni.ItemHeight = 20;
-            this.listBoxZaposleni.Location = new System.Drawing.Point(12, 368);
+            this.listBoxZaposleni.Location = new System.Drawing.Point(12, 488);
             this.listBoxZaposleni.Name = "listBoxZaposleni";
-            this.listBoxZaposleni.Size = new System.Drawing.Size(544, 284);
+            this.listBoxZaposleni.Size = new System.Drawing.Size(550, 404);
             this.listBoxZaposleni.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageOrdinacije);
+            this.tabControl1.Controls.Add(this.tabPageZaposleni);
+            this.tabControl1.Location = new System.Drawing.Point(568, 488);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(550, 404);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPageOrdinacije
+            // 
+            this.tabPageOrdinacije.Controls.Add(this.btnOrdinacijeUpdate);
+            this.tabPageOrdinacije.Controls.Add(this.btnOrdinacijeVnos);
+            this.tabPageOrdinacije.Controls.Add(this.label4);
+            this.tabPageOrdinacije.Controls.Add(this.label3);
+            this.tabPageOrdinacije.Controls.Add(this.label2);
+            this.tabPageOrdinacije.Controls.Add(this.textBoxOrdinacijaNaslov);
+            this.tabPageOrdinacije.Controls.Add(this.textBoxOrdinacijaIme);
+            this.tabPageOrdinacije.Controls.Add(this.comboBox1);
+            this.tabPageOrdinacije.Location = new System.Drawing.Point(4, 29);
+            this.tabPageOrdinacije.Name = "tabPageOrdinacije";
+            this.tabPageOrdinacije.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageOrdinacije.Size = new System.Drawing.Size(542, 371);
+            this.tabPageOrdinacije.TabIndex = 0;
+            this.tabPageOrdinacije.Text = "Ordinacije";
+            this.tabPageOrdinacije.UseVisualStyleBackColor = true;
+            // 
+            // tabPageZaposleni
+            // 
+            this.tabPageZaposleni.Location = new System.Drawing.Point(4, 29);
+            this.tabPageZaposleni.Name = "tabPageZaposleni";
+            this.tabPageZaposleni.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageZaposleni.Size = new System.Drawing.Size(536, 251);
+            this.tabPageZaposleni.TabIndex = 1;
+            this.tabPageZaposleni.Text = "Zaposleni";
+            this.tabPageZaposleni.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(76, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBoxOrdinacijaIme
+            // 
+            this.textBoxOrdinacijaIme.Location = new System.Drawing.Point(76, 4);
+            this.textBoxOrdinacijaIme.Name = "textBoxOrdinacijaIme";
+            this.textBoxOrdinacijaIme.Size = new System.Drawing.Size(234, 26);
+            this.textBoxOrdinacijaIme.TabIndex = 1;
+            // 
+            // textBoxOrdinacijaNaslov
+            // 
+            this.textBoxOrdinacijaNaslov.Location = new System.Drawing.Point(76, 50);
+            this.textBoxOrdinacijaNaslov.Name = "textBoxOrdinacijaNaslov";
+            this.textBoxOrdinacijaNaslov.Size = new System.Drawing.Size(234, 26);
+            this.textBoxOrdinacijaNaslov.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ime: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Naslov: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Kraj: ";
+            // 
+            // btnOrdinacijeVnos
+            // 
+            this.btnOrdinacijeVnos.AutoSize = true;
+            this.btnOrdinacijeVnos.Location = new System.Drawing.Point(76, 235);
+            this.btnOrdinacijeVnos.Name = "btnOrdinacijeVnos";
+            this.btnOrdinacijeVnos.Size = new System.Drawing.Size(75, 30);
+            this.btnOrdinacijeVnos.TabIndex = 6;
+            this.btnOrdinacijeVnos.Text = "Vnos";
+            this.btnOrdinacijeVnos.UseVisualStyleBackColor = true;
+            // 
+            // btnOrdinacijeUpdate
+            // 
+            this.btnOrdinacijeUpdate.AutoSize = true;
+            this.btnOrdinacijeUpdate.Location = new System.Drawing.Point(235, 235);
+            this.btnOrdinacijeUpdate.Name = "btnOrdinacijeUpdate";
+            this.btnOrdinacijeUpdate.Size = new System.Drawing.Size(85, 30);
+            this.btnOrdinacijeUpdate.TabIndex = 7;
+            this.btnOrdinacijeUpdate.Text = "Posodobi";
+            this.btnOrdinacijeUpdate.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -114,7 +234,8 @@ namespace veterinarske_ordinacije
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(1412, 703);
+            this.ClientSize = new System.Drawing.Size(1149, 915);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.listBoxZaposleni);
             this.Controls.Add(this.groupBoxSearch);
             this.Controls.Add(this.listBoxOrdinacije);
@@ -124,6 +245,9 @@ namespace veterinarske_ordinacije
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBoxSearch.ResumeLayout(false);
             this.groupBoxSearch.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageOrdinacije.ResumeLayout(false);
+            this.tabPageOrdinacije.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -137,6 +261,17 @@ namespace veterinarske_ordinacije
         private System.Windows.Forms.GroupBox groupBoxSearch;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.ListBox listBoxZaposleni;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageOrdinacije;
+        private System.Windows.Forms.TabPage tabPageZaposleni;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxOrdinacijaNaslov;
+        private System.Windows.Forms.TextBox textBoxOrdinacijaIme;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnOrdinacijeUpdate;
+        private System.Windows.Forms.Button btnOrdinacijeVnos;
     }
 }
 
